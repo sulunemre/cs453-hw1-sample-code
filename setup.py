f4ebfde730383b52121dc3bf5e954c3331b8c916
Alp Uneri <alpuneri1560@gmail.com>
2022-03-08 13:39:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
-    name="Alp Uneri CI/CD",
+    name="AlpUneriCICD",
     author="Alp Üneri",  # TODO: Write your name
     version="2.0.0",
     packages=["dictionary"],
